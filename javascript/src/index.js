96ce0e27df589d79c41203e2f1eaf7adea55d21c
dino <dinis_santos@eticalgarve.com>
2026-01-27 22:05:33 +0000

    feat/ primeiro trabalho feito em javascript com docker funcional amen

---
+++ javascript/src/index.js
@@ -0,0 +1,17 @@
+let numeroInserido = 15
+
+
+if(numeroInserido>=10 && numeroInserido<=20 ){
+
+    if(numeroInserido!==15){
+        console.log("O número é menor que 20 e maior que 10!")
+    }
+
+    console.log("O numero é 15")
+}
+
+
+else if(numeroInserido>20 || numeroInserido<10){
+    console.log("O número é invalido")
+}
+
