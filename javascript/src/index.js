8773389ef80225f0bf28f13ebc77963af713d7b3
dino <dinis_santos@eticalgarve.com>
2026-01-28 20:29:20 +0000

    feat/final touches on second exercise to make it a function and make text a little more fun

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -1,12 +1,17 @@
 const prompt = require("prompt-sync")();
 
-let numeroInserido = Number(prompt("Insere um número: "));
 
-if(numeroInserido%5===0 && numeroInserido<20 && numeroInserido>1){
-    console.log("O número é multiplo de 5 entre 5 e 15")
-}else if(numeroInserido>20 || numeroInserido<1){
-    console.log("Número inválido")
-}else{
-    console.log("Número válido entre 0 e 20 sem ser multiplo de 5")
+function tudo(){
+    let numeroInserido = Number(prompt("Insere um número: "));
+
+    if(numeroInserido%5===0 && numeroInserido<20 && numeroInserido>1){
+        console.log("PARE, o seu número é multiplo de 5 menor que 20, bueda fixe")
+    }else if(numeroInserido>20 || numeroInserido<1){
+        console.log("Número inválido")
+    }else{
+        console.log("Número válido entre 0 e 20")
+    }
+
 }
 
+tudo()
