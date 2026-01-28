65c1eb1efe644bda2bb9d13e67954139a6df9675
dino <dinis_santos@eticalgarve.com>
2026-01-28 20:00:38 +0000

    feat/segundo exercicio feito, usando prompt e sua instalacao toda feita

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -1,17 +1,12 @@
-let numeroInserido = 15
+const prompt = require("prompt-sync")();
 
+let numeroInserido = Number(prompt("Insere um número: "));
 
-if(numeroInserido>=10 && numeroInserido<=20 ){
-
-    if(numeroInserido!==15){
-        console.log("O número é menor que 20 e maior que 10!")
-    }
-
-    console.log("O numero é 15")
-}
-
-
-else if(numeroInserido>20 || numeroInserido<10){
-    console.log("O número é invalido")
+if(numeroInserido%5===0 && numeroInserido<20 && numeroInserido>1){
+    console.log("O número é multiplo de 5 entre 5 e 15")
+}else if(numeroInserido>20 || numeroInserido<1){
+    console.log("Número inválido")
+}else{
+    console.log("Número válido entre 0 e 20 sem ser multiplo de 5")
 }
 
