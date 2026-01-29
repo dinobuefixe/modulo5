79b1d13114cb23f41d1528a143332d8c0bcf1d3f
dino <dinis_santos@eticalgarve.com>
2026-01-29 19:39:41 +0000

    chore/return in tudo function

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -3,11 +3,14 @@ const prompt = require("prompt-sync")();
 
 function tudo(numeroInserido){
 
-    numeroInserido % 5 === 0 && numeroInserido < 20 && numeroInserido > 1
-        ? console.log("PARE, o seu número é múltiplo de 5 menor que 20, bueda fixe")
-        : numeroInserido > 20 || numeroInserido < 1
-            ? console.log("Número inválido")
-            : console.log("Número válido entre 0 e 20 inclusive");
+    if (numeroInserido % 5 === 0 && numeroInserido < 20 && numeroInserido > 1){
+        console.log("PARE, o seu número é múltiplo de 5 menor que 20, bueda fixe")
+        return
+    }
+
+    numeroInserido > 20 || numeroInserido < 1
+        ? console.log("Número inválido")
+        : console.log("Número válido entre 0 e 20 inclusive");
 }
 
 
