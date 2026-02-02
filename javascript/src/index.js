d4f78274cf06548d055620c4487b090a6821d600
dino <dinis_santos@eticalgarve.com>
2026-02-02 20:27:45 +0000

    feat/listas

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -1,18 +1,31 @@
-const prompt = require("prompt-sync")();
+// const prompt = require("prompt-sync")();
 
 
-function tudo(numeroInserido){
+// function tudo(numeroInserido){
 
-    if (numeroInserido % 5 === 0 && numeroInserido < 20 && numeroInserido > 1){
-        console.log("PARE, o seu número é múltiplo de 5 menor que 20, bueda fixe")
-        return
-    }
+//     if (numeroInserido % 5 === 0 && numeroInserido < 20 && numeroInserido > 1){
+//         console.log("PARE, o seu número é múltiplo de 5 menor que 20, bueda fixe")
+//         return
+//     }
 
-    numeroInserido > 20 || numeroInserido < 1
-        ? console.log("Número inválido")
-        : console.log("Número válido entre 0 e 20 inclusive");
-}
+//     numeroInserido > 20 || numeroInserido < 1
+//         ? console.log("Número inválido")
+//         : console.log("Número válido entre 0 e 20 inclusive");
+// }
 
 
-let numeroInserido = Number(prompt("Insere um número: "));
-tudo(numeroInserido)
+// let numeroInserido = Number(prompt("Insere um número: "));
+// tudo(numeroInserido)
+
+// const myList = ["a", "b"];
+
+// myList.push("c");
+
+// myList[0] = "d";
+
+// myList.reverse();
+
+// console.log(myList)
+
+
+
